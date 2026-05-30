d72bad65e01a76e8e0469ec5876bb111d90d1d71
Suman Bhattacharjee <sumanbhattacharjee807@gmail.com>
2026-05-30 19:39:30 +0530

    Initial commit

---
+++ Resume-Analyzer/src/test/java/com/sb/Resume/Analyzer/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.Resume.Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
